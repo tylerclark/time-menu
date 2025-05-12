fc58e39b3b3b7e13476ff5252a264da01be0fbc8
Tyler Clark <tyler@tylerclark.com>
2025-05-12 00:25:26 -0400

    Add recording icon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { menubar } = require("menubar");
 const path = require("path");
 
 const mb = menubar({
-  index: "https://time.clarkinteractive.com",
+  index: "https://time.clarkinteractive.com?special=1",
   icon: path.join(__dirname, "icon@2x.png"),
   browserWindow: {
     width: 700,
@@ -16,4 +16,14 @@ const mb = menubar({
 
 mb.on("ready", () => {
   console.log("Menubar app is ready!");
+});
+
+mb.on("after-create-window", () => {
+  mb.window.webContents.on("did-navigate-in-page", (event, url) => {
+    if (url.includes("running=1")) {
+      mb.tray.setImage(path.join(__dirname, "icon-special@2x.png"));
+    } else {
+      mb.tray.setImage(path.join(__dirname, "icon@2x.png"));
+    }
+  });
 });
